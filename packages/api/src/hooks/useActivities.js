d42494fbfccfe3a81db210aeb206846be3484c9d
William Wong <compulim@hotmail.com>
2021-03-12 04:18:21 -0800

    Fix useActivities

--- packages/api/src/hooks/useActivities.js
+++ packages/api/src/hooks/useActivities.js
@@ -1,10 +1,20 @@
+import { useContext } from 'react';
+
 import { useSelector } from './internal/WebChatReduxContext';
-import useWebChatSpeechContext from './internal/WebChatSpeechContext';
+import WebChatSpeechContext from './internal/WebChatSpeechContext';
 
 // TODO: Add options to support: "all" (default), "render" and "speechsynthesis".
 export default function useActivities(options = 'all') {
   const allActivities = useSelector(({ activities }) => activities);
-  const { synthesizingActivities } = useWebChatSpeechContext();
+  const speechContext = useContext(WebChatSpeechContext);
 
-  return [options === 'speechsynthesis' ? synthesizingActivities : allActivities];
+  if (options === 'speechsynthesis') {
+    if (!speechContext) {
+      throw new Error('This hook can only be used on a component that is a descendant of <Composer>');
+    }
+
+    return [speechContext.synthesizingActivities];
+  }
+
+  return [allActivities];
 }
--- packages/api/src/utils/getActivityKey.js
+++ packages/api/src/utils/getActivityKey.js
@@ -1,3 +1,9 @@
 export default function getActivityKey(activity) {
-  return activity && (activity.key || (activity.channelData && activity.channelData.clientActivityID) || activity.id);
+  if (!activity) {
+    return;
+  }
+
+  const { channelData: { clientActivityID, 'webchat:key': key } = {}, id } = activity;
+
+  return key || clientActivityID || id;
 }
