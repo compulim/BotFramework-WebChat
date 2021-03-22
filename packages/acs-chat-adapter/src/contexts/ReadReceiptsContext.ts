efe0112c8e33654f16a92de593f66db8304b217e
William Wong <compulim@hotmail.com>
2021-03-21 19:29:44 -0700

    Clean up

--- packages/acs-chat-adapter/src/contexts/ReadReceiptsContext.ts
+++
@@ -1,9 +0,0 @@
-// import { createContext } from 'react';
-
-// import { WebChatReadReceipts } from '../types/WebChatReadReceipts';
-
-// const context = createContext<WebChatReadReceipts>(undefined);
-
-// context.displayName = 'ACSChatAdapter.ReadReceiptsContext';
-
-// export default context;
